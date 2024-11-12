17477ede237b9191a4272a21bda0f6e9f5aea1d4
Taneristique <48108872+Taneristique@users.noreply.github.com>
2024-11-12 18:38:47 +0300

    comment added

--- src/contracts/Square.sol
+++ src/contracts/Square.sol
@@ -4,7 +4,8 @@ pragma solidity 0.8.26;
 contract Square {
     error willOverflow();
     function square(uint256 number) public pure returns(uint256){
-        if(number > 0xffffffffffffffffffffffffffffffff){
+        if(number > 0xffffffffffffffffffffffffffffffff){ 
+            /*The product would overflow if value > 340282366920938463463374607431768211455*/
             revert willOverflow();
         }
         return number * number;
